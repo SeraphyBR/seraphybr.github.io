0a126671885ed84be9ad3f18c1139ff967d5fe5f
SeraphyBR <luisjuniorbr@gmail.com>
2021-04-27 15:13:34 -0300

    Initial setup

---
+++ shell.nix
@@ -0,0 +1,26 @@
+# Use nixpkgs with oxalica rust-bin overlay
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in
+# Avoid typing `nixpkgs.` before each package name
+with nixpkgs;
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [ stdenv zlib pkg-config openssl ];
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+      rust_channel
+      cargo-edit
+      cargo-make
+      microserver
+      # Use steam-run, for a fhs enviroment, allow vscode code-lldb debugger to run
+      # $ steam-run code .
+      (steam.override {
+        extraPkgs = pkgs: [ pkg-config zsh ];
+        extraLibraries = pkgs: [ zlib openssl ];
+        nativeOnly = true;
+      }).run
+  ];
+}
