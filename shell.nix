3ecd5697de95ed87153c5d3169d9a931dba0a076
SeraphyBR <luisjuniorbr@gmail.com>
2021-05-04 15:02:14 -0300

    Remove extension in Rust toolchain file, for azure

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+  rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
 in
 # Avoid typing `nixpkgs.` before each package name
 with nixpkgs;
