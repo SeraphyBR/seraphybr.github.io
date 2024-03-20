d72b88d10ae0caea90a80383a7325389a6027270
SeraphyBR <luisjuniorbr@gmail.com>
2024-03-20 14:17:01 -0300

    add font awesome icons

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -42,6 +42,7 @@ pub fn Home() -> impl IntoView {
                 <h1>"Welcome to Leptos"</h1>
 
                 <p class="text-xl bg-slate-100">"Teste tailwind"</p>
+                <i class="fa-solid fa-blog"></i>
 
                 <div class="buttons">
                     <Button/>
