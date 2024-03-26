a127084f3a0c5062ad22e4e974928df9c1ccf303
Luiz Junio <luisjuniorbr@gmail.com>
2024-03-26 20:52:45 +0000

    Adicionando transition no button

--- src/components/button.rs
+++ src/components/button.rs
@@ -5,7 +5,7 @@ use leptos_router::A;
 pub fn LinkBtn(#[prop(into)] href: String, children: Children) -> impl IntoView {
     view! {
         <A href
-            class="tw-text-sm tw-font-bold tw-border-solid tw-border-2 tw-rounded-xl tw-max-w-fit tw-px-4 tw-py-2 hover:tw-text-greenLime"
+            class="tw-text-sm tw-font-bold tw-border-solid tw-border-2 tw-rounded-xl tw-max-w-fit tw-px-4 tw-py-2 tw-transition tw-duration-200 hover:tw-text-greenLime"
         >
             {children()}
         </A>
